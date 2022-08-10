7f625a756180a69d29a6e9fb94aa57ba518c0458
d1once <dionisenja@gmail.com>
2022-08-10 18:33:17 +0200

    s20

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,36 @@
+using API.Entities;
+using API.Entities.OrderAggregate;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : IdentityDbContext<User, Role, int>
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .HasOne(a => a.Address)
+                .WithOne()
+                .HasForeignKey<UserAddress>(a => a.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Role>()
+                .HasData(
+                    new Role { Id = 1, Name = "Member", NormalizedName = "MEMBER" },
+                    new Role { Id = 2, Name = "Admin", NormalizedName = "ADMIN" }
+                );
+        }
+    }
+}

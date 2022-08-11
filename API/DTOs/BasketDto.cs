efe1c2f1320518fb7dc13596ae229dbe339c6f48
d1once <dionisenja@gmail.com>
2022-08-11 21:41:52 +0200

    s20

---
+++ API/DTOs/BasketDto.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace API.DTOs
+{
+    public class BasketDto
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public List<BasketItemDto> Items { get; set; }
+        public string PaymentIntentId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}

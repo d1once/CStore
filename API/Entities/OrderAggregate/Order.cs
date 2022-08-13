5e354311f3c5497ae663cf3f7762896e5ab6831a
d1once <dionisenja@gmail.com>
2022-08-13 08:07:11 +0200

    s20

---
+++ API/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace API.Entities.OrderAggregate
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public ShippingAddress ShippingAddress { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public List<OrderItem> OrderItems { get; set; }
+        public long Subtotal { get; set; }
+        public long DeliveryFee { get; set; }
+        public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+        public string PaymentIntentId { get; set; }
+        
+        public long GetTotal()
+        {
+            return Subtotal + DeliveryFee;
+        }
+    }
+}

5f7ede6243feabd12348f4d480c2fb16c48f9586
d1once <dionisenja@gmail.com>
2022-08-14 20:22:35 +0200

    s20

---
+++ API/Extensions/BasketExtensions.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class BasketExtensions
+    {
+        public static BasketDto MapBasketToDto(this Basket basket)
+        {
+            return new BasketDto
+            {
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                PaymentIntentId = basket.PaymentIntentId,
+                ClientSecret = basket.ClientSecret,
+                Items = basket.Items.Select(item => new BasketItemDto
+                {
+                    ProductId = item.ProductId,
+                    Name = item.Product.Name,
+                    Price = item.Product.Price,
+                    PictureUrl = item.Product.PictureUrl,
+                    Type = item.Product.Type,
+                    Brand = item.Product.Brand,
+                    Quantity = item.Quantity
+                }).ToList()
+            };
+        }
+
+        public static IQueryable<Basket> RetrieveBasketWithItems(this IQueryable<Basket> query, string buyerId)
+        {
+            return query.Include(i => i.Items).ThenInclude(p => p.Product).Where(b => b.BuyerId == buyerId);
+        }
+    }
+}

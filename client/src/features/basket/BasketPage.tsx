3c148def290bc4353bb347469c46b4104915cb1b
d1once <dionisenja@gmail.com>
2022-10-01 20:57:34 +0200

    s23

---
+++ client/src/features/basket/BasketPage.tsx
@@ -0,0 +1,33 @@
+import { Button, Grid, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useAppSelector } from "../../app/store/configureStore";
+import BasketSummary from "./BasketSummary";
+import BasketTable from "./BasketTable";
+
+export default function BasketPage() {
+    const { basket } = useAppSelector(state => state.basket);
+
+    if (!basket) return <Typography variant='h3'>Your basket is empty</Typography>
+
+    return (
+        <>
+            <BasketTable items={basket.items} />
+            <Grid container>
+                <Grid item xs={6} />
+                <Grid item xs={6}>
+                    <BasketSummary />
+                    <Button
+                        component={Link}
+                        to='/checkout'
+                        variant='contained'
+                        size='large'
+                        fullWidth
+                    >
+                        Checkout
+                    </Button>
+                </Grid>
+            </Grid>
+        </>
+
+    )
+}

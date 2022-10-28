e39c4b395aadc45eb1bab5c30e2c6735a4b26fd0
d1once <dionisenja@gmail.com>
2022-10-28 20:34:32 +0200

    s25

---
+++ client/src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app/layout/styles.css';
+import App from './app/layout/App';
+import reportWebVitals from './reportWebVitals';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export const history = createBrowserHistory();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router history={history}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
